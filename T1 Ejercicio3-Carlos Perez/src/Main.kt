ab8de9de52bde591a090cb915e0296f0d009c2af
carloospg <carlosperezgosalvez@gmail.com>
2024-11-28 12:33:50 +0100

    ejemploCambiado

--- T1 Ejercicio3-Carlos Perez/src/Main.kt
+++ T1 Ejercicio3-Carlos Perez/src/Main.kt
@@ -1,9 +1,7 @@
 fun main() {
-    println("Voy a calcular el area y la circunferencia de un circulo")
-    println("Dame el valor del radio del circulo")
-    var r: Int = readln().toInt()
-    var a = r * 3.14
-    var c = 2 * r * 3.14
-    println("El area del circulo es de: $a cm")
-    println("La circunferencia del circulo es de: $c cm")
+    println("Voy a calcular el area de un cuadrado")
+    println("Dame el valor del lado del cuadrado")
+    var l: Int = readln().toInt()
+    var a = l * l
+    println("El area del cuadrado es de: $a cm")
 }
